ae09388291a08ad6cd46ebf8bef438d8b11aa499
Marcos <marcos2005.machado@gmail.com>
2023-09-05 11:15:40 -0300

    Implementei a classe abstrata Bot

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,29 +5,34 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(self, nome):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome, comandos:list[tuple[str]]):
+        self.__nome = nome
+        self.__comandos = comandos
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self.__nome
 
-    #nao esquecer o decorator
+    @nome.setter
     def nome(self, nome):
-        pass
+        self.__nome=nome
 
     def mostra_comandos(self):
-        pass
+        for i in range(len(self.__comandos)):
+            print(f'{i+1}-{self.__comandos[i][0]}')
+        
 
     @abstractmethod
     def executa_comando(self,cmd):
-        pass
+        if cmd<=len(self.__comandos) and cmd>=1:
+            print(self.__comandos[cmd][1])
+        else:
+            print('Comando Inválido!') #mudardepois
 
     @abstractmethod
-    def boas_vindas():
+    def boas_vindas(self):
         pass
     
     @abstractmethod
-    def despedida():
+    def despedida(self):
         pass
