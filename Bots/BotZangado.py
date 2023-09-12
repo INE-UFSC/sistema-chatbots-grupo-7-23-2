a40bf859d6209440f90edbcb2b486fde66d9c14a
Marcos Machado <marcos2005.machado@gmail.com>
2023-09-12 09:49:06 -0300

    Terminei o bot Zangado

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -3,18 +3,18 @@ from Bots.Bot import Bot
 class BotZangado(Bot):
     def __init__(self, nome):
         comandos=[
-            ('Olá!','a'),
-            ('Me conte sobre você','p'),
-            (),
-            ()
+            ('Olá!','Não fale comigo, idiota!'),
+            ('Me conte sobre você',f'Eu sou {self.nome}, e eu odeio conversar com você!'),
+            ('Como você está?','Com você por aqui? Horrível!'),
+            ('Você quer ser meu amigo?','Isso é uma pergunta séria? Suma da minha frente!')
         ]
         super().__init__(nome,comandos)
 
     def apresentacao(self):
-        return ''
+        return f'Ah, outro humano insuportável! Vá embora.'
 
     def boas_vindas(self):
-        return ''
+        return 'O que você pensa que está fazendo? Deixe-me em paz!'
 
     def despedida(self):
-        return ''
+        return 'Conversar com você foi uma total perda de tempo! Adeus.'
